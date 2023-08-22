1448e3ac9d1f5e199244437a5f881a2679b6ee4b
reinoldes <reinoldesxbox@gmail.com>
2023-08-22 16:02:40 -0300

    Aula 01, listas

---
+++ Listas.py
@@ -0,0 +1,57 @@
+idades = [39, 30, 27, 18]
+
+print (idades)
+
+print (len (idades))
+
+print (idades [3])
+
+idades.append (15)
+
+print (idades)
+
+for idade in idades:
+    print (idade)
+
+idades.remove(30)
+
+print (idades)
+
+print (28 in idades)
+
+if 15 in idades:
+    idades.remove(15)
+    print(idades)
+
+if 27 in idades:
+    idades.insert (0, 20)
+    print (idades)
+
+if 20 in idades:
+    idades.extend([27,19])
+    print (idades)
+
+idades_ano_que_vem = []
+
+# Dessa forma o codigo anterior da bug
+#for idade in idades:
+#    idades_ano_que_vem.append(idade+1)
+#    print (idades_ano_que_vem)
+
+print ([(idade+1) for idade in idades])
+
+print (sorted(idades))
+print (idades)
+
+print ([(idade) for idade in idades if idade > 21])
+
+def faz_processamento_de_visualizacao(lista = None):
+    if lista == None:
+        lista = list()
+    print(len(lista))
+    print(lista)
+    lista.append(13)
+
+teste = faz_processamento_de_visualizacao
+
+print (teste)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Collections
+
+
+Aula 01 Listas e operações
+-----------------------------
+
+Introdção a listas e operações dentro das listas
